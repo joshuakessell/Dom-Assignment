56572fc4ca3da1b8534fff6054e9c631eeddad89
JoshYewUhh <jckessell@gmail.com>
2018-09-26 11:27:39 -0500

    Updated Add Functions

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
+
+
+
 const setView = function(e) {
     e.preventDefault();
     $('#contentArea').empty();
@@ -8,25 +11,26 @@ const setView = function(e) {
 }
 
 
-const addContact = function(e) {
-    const newContact = {
-        name: document.forms.newname,
-        officeNum: document.forms.newoffice,
-        phoneNum: document.forms.newphone
-    }
-    employeeList.push(newContact);
-
-}
-
 const setAdd = function(e) {
     e.preventDefault();
     $('#contentArea').empty();
     
-    $('#contentArea').html(`<form name ="addForm">Name: <input type="text" name="newname">Office Number: <input type="text" name="newoffice">Phone Number: <input type="text" name="newphone"><input type ="button" id="addContact" class="btn" value="+" onclick="addContact"></form>`);
-
     for (let i = 0; i < employeeList.length; i++) {
         $('#contentArea').append(`<div class="content-row"><div class="col-md-6" id="viewcell" value=${i}> Name: ${employeeList[i].name}<br><br> Office: ${employeeList[i].officeNum}<br><br> Phone: ${employeeList[i].phoneNum}</div><div class="col-md-6" id="spacercell"></div>`);
     }
+
+    const addContact = function(e) {
+        
+        const newContact = {
+            name: $("#newname").val(),
+            officeNum: parseInt($("#newoffice").val()),
+            phoneNum: $("#newphone").val()
+            }
+        employeeList.push(newContact);
+        console.log(employeeList);
+    }
+
+    $('#addContact').on('click', addContact);
 }
 
 
@@ -61,7 +65,7 @@ const setDelete = function(e) {
 //event listeners
 $('.view').on('click', setView);
 $('.add').on('click', setAdd);
-$('.addContact').on('click', addContact);
+
 $('.verify').on('click', setVerify);
 $('.update').on('click', setUpdate);
 $('.delete').on('click', setDelete);
