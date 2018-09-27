c73d6f49c7b1c3d6836778abcc7f38a57f81fd1c
JoshYewUhh <jckessell@gmail.com>
2018-09-26 20:42:40 -0500

    added verify functionality

--- app.js
+++ app.js
@@ -4,30 +4,42 @@
 
 const setView = function(e) {
     e.preventDefault();
-    $('#contentArea').empty();
+    $('.contentArea').empty();
+    $('.formRow').empty();
+
     for (let i = 0; i < employeeList.length; i++) {
-        $('#contentArea').append(`<div class="content-row"><div class="col-md-6" id="viewcell" value=${i}> Name: ${employeeList[i].name}<br><br> Office: ${employeeList[i].officeNum}<br><br> Phone: ${employeeList[i].phoneNum}</div><div class="col-md-6" id="spacercell"></div>`);
+        $('.contentArea').append(`<div class="content-row"><div class="col-md-6" id="viewcell" value=${i}> Name: ${employeeList[i].name}<br><br> Office: ${employeeList[i].officeNum}<br><br> Phone: ${employeeList[i].phoneNum}</div><div class="col-md-6" id="spacercell"></div>`);
     }
 }
 
 
 const setAdd = function(e) {
     e.preventDefault();
-    $('#contentArea').empty();
+    $('.contentArea').empty();
+    $('.formRow').empty();
+
+    $('.formRow').html(`<form name ="addForm">
+        Name: 
+        <input type="text" id="newname" >
+        Office Number: 
+        <input type="text" id="newoffice">
+        Phone Number: 
+        <input type="text" id="newphone">
+        <input type ="button" id="addContact" class="btn" value="+" onclick="addContact">
+        </form>`);
     
     for (let i = 0; i < employeeList.length; i++) {
-        $('#contentArea').append(`<div class="content-row"><div class="col-md-6" id="viewcell" value=${i}> Name: ${employeeList[i].name}<br><br> Office: ${employeeList[i].officeNum}<br><br> Phone: ${employeeList[i].phoneNum}</div><div class="col-md-6" id="spacercell"></div>`);
+        $('.contentArea').append(`<div class="content-row"><div class="col-md-6" id="viewcell" value=${i}> Name: ${employeeList[i].name}<br><br> Office: ${employeeList[i].officeNum}<br><br> Phone: ${employeeList[i].phoneNum}</div><div class="col-md-6" id="spacercell"></div>`);
     }
 
     const addContact = function(e) {
-        
         const newContact = {
             name: $("#newname").val(),
             officeNum: parseInt($("#newoffice").val()),
             phoneNum: $("#newphone").val()
             }
         employeeList.push(newContact);
-        console.log(employeeList);
+        alert(`${newContact.name} has been successfully added!`);
     }
 
     $('#addContact').on('click', addContact);
@@ -39,11 +51,30 @@ const setAdd = function(e) {
 
 const setVerify = function(e) {
     e.preventDefault();
-    $('#contentArea').empty();
-
+    $('.contentArea').empty();
+    $('.formRow').empty();
+    $('.formRow').html(`<form name ="verifyForm">
+        Name: 
+        <input type="text" id="newname">
+        <input type ="button" id="setVerify" class="btn" value="Verify" onclick="setVerify">
+        </form>`);
+        
+    const verifyContact = function(e) {
+        $('.contentArea').empty();
+        let nameCheck = $("#newname").val();
+        let flag = true;
+        for(let i=0;i<employeeList.length; i++) {
+            if(nameCheck === employeeList[i].name){
+                $(".contentArea").append("Yes");
+                flag = false;
+            }
+        }
+        if (flag) {
+            $(".contentArea").append("No");
+        }
+    }  
+    $('#setVerify').on('click', verifyContact);
 }
-
-
 
 
 const setUpdate = function(e) {
